2c511a7d3284680cf4ea13c9761e763e6f98866a
Nikhil Ganta <nikhilganta95@gmail.com>
2017-09-15 21:45:34 +0530

    Add files via upload

--- js/markers.js
+++ js/markers.js
@@ -1,52 +1,91 @@
-var locations = [
-  {
-    title: 'Domino\'s Pizza',
-    location: {lat: 17.724756, lng: 83.314267},
-    type: 'Restaurant'
-  },
-  {
-    title: 'Ibaco Ice Cream Parlour',
-    location: {lat: 17.717266, lng: 83.329172},
-    type: 'Restaurant'
-  },
-  {
-    title: 'Diamond Park',
-    location: {lat: 17.726823, lng: 83.303521},
-    type: 'Park'
-  },
-  {
-    title: 'INS Kursura Submarine Museum',
-    location: {lat: 17.717266, lng: 83.330087},
-    type: 'Museum'
-  },
-  {
-    title: 'Mathsya Darshini',
-    location: {lat: 17.712769, lng: 83.319919},
-    type: 'Aquarium'
-  },
-  {
-    title: 'Novotel',
-    location: {lat: 17.710699, lng: 83.315891},
-    type: 'Hotel'
-  },
-  {
-    title: 'VUDA City Central Park',
-    location: {lat: 17.722811, lng: 83.306632},
-    type: 'Park'
-  },
-  {
-    title: 'Andhra University',
-    location: {lat:17.728816, lng: 83.320085},
-    type: 'University'
-  },
-  {
-    title: 'INOX Chitralaya',
-    location: {lat: 17.712391,lng: 83.301348},
-    type: 'Theatre'
-  },
-  {
-    title: 'Barbeque Nation',
-    location: {lat: 17.717452,lng: 83.318552},
-    type: 'Restaurant'
-  }
+var locations = [{
+        title: 'Domino\'s Pizza',
+        location: {
+            lat: 17.724756,
+            lng: 83.314267
+        },
+        type: 'Restaurant',
+        fsID: '4de7a603d22dc38f1ee5d32f'
+    },
+    {
+        title: 'Ibaco Ice Cream Parlour',
+        location: {
+            lat: 17.717266,
+            lng: 83.329172
+        },
+        type: 'Restaurant',
+        fsID: '572f6a0d498ec2eb8979c9fc'
+    },
+    {
+        title: 'Diamond Park',
+        location: {
+            lat: 17.726823,
+            lng: 83.303521
+        },
+        type: 'Park',
+        fsID: '4ebead790e019a162a3d8f35'
+    },
+    {
+        title: 'INS Kursura Submarine Museum',
+        location: {
+            lat: 17.717266,
+            lng: 83.330087
+        },
+        type: 'Museum',
+        fsID: '4ef7c378490124bf99bf70e0'
+    },
+    {
+        title: 'Mathsya Darshini',
+        location: {
+            lat: 17.712769,
+            lng: 83.319919
+        },
+        type: 'Aquarium',
+        fsID: '510d2487e4b0a910c71b3d63'
+    },
+    {
+        title: 'Novotel',
+        location: {
+            lat: 17.710699,
+            lng: 83.315891
+        },
+        type: 'Hotel',
+        fsID: '4f90f1991081461b19750a40'
+    },
+    {
+        title: 'VUDA City Central Park',
+        location: {
+            lat: 17.722811,
+            lng: 83.306632
+        },
+        type: 'Park',
+        fsID: '582b0ec77a8342227e7de5a5'
+    },
+    {
+        title: 'Andhra University',
+        location: {
+            lat: 17.728816,
+            lng: 83.320085
+        },
+        type: 'University',
+        fsID: '4eaa77fe99116a4249db40bc'
+    },
+    {
+        title: 'INOX Chitralaya',
+        location: {
+            lat: 17.712391,
+            lng: 83.301348
+        },
+        type: 'Theatre',
+        fsID: '53b6708a498e4b19feecc2f6'
+    },
+    {
+        title: 'Barbeque Nation',
+        location: {
+            lat: 17.717452,
+            lng: 83.318552
+        },
+        type: 'Restaurant',
+        fsID: '552011f1498e9ac948e7ed11'
+    }
 ];
